d5bf83fcbc5e4aa84d01040333a8e8b807da071d
euphoriiah <euphorya@outlook.jp>
2022-08-31 23:23:08 +0200

    Add script to display products on the main page

--- front/js/script.js
+++ front/js/script.js
@@ -1,7 +1,29 @@
-//Show products on the main page//
 
-fillSection();
-async function getArticles() {
-    var retrieveItems = await fetch("http://localhost:3000/api/products")
-    return await retrieveItems.json();
+//Recover products from the API//
+const recoverProducts = async function () {
+    await fetch("http://localhost:3000/api/products")
+        .then(function (res) {
+            return res.json();
+        })
+        .then(function (data) {
+            return (products = data);
+        });
+};
+// Select element and display products //
+const productElement = document.querySelector("#items");
+
+//Display products on the main page //
+async function displayProducts() {
+    await recoverProducts();
+    products.forEach((product) => {
+        productElement.innerHTML += `
+            <a href="./product.html?id=${product._id}">
+            <article>
+                <img src="${product.imageUrl}" alt="${product.altTxt}" />
+                <h3 class="productName">${product.name}</h3>
+                <p class="productDescription">${product.description}</p>  
+            </article>
+            </a>`;
+    });
 }
+displayProducts();
