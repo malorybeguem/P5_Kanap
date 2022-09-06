e0d9a2098227d635c3f86d44749c32743d2b6d24
malory.beguem <euphorya@outlook.jp>
2022-09-07 00:08:07 +0200

    major changes on all pages

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,12 +1,11 @@
-const urlOrderId = new URLSearchParams(window.location.search).get("orderId");
-if(urlOrderId === null || urlOrderId === ""){
-    alert ("Une erreur s'est produite lors de la validation de votre commande. Veuillez nous en excuser !");
-    window.location.href = "index.html";
- }
- else{
-    // Sélection de l'élément html dans lequel on veut afficher le numéro de commande
-    const idCommande = document.getElementById("orderId");
-    // On insère le numéro de commande dans le html
-    idCommande.innerText = urlOrderId;
-    console.log(idCommande);
-}
+//Recovering Order Number
+
+let str = new URL(window.location.href);
+let productId = str.searchParams.get("id");
+
+// Displaying the number
+const userOrderId = document.querySelector('#orderId');
+userOrderId.textContent = productId;
+
+// Reset datas on localStorage
+localStorage.removeItem('userProducts');
